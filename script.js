a988e7d6de19e4847401fe52f1f890859874655c
varshitha246 <varshithakagithala@gmail.com>
2025-04-13 16:40:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+let display = document.getElementById("display");
+
+function append(char) {
+  display.value += char;
+}
+
+function clearDisplay() {
+  display.value = "";
+}
+
+function deleteChar() {
+  display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch (e) {
+    display.value = "Error";
+  }
+}
